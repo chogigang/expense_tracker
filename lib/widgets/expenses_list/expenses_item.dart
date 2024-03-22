a70cff3612b702a513882c3c09c347ca518dfeae
chogigang <cbh2031@gmail.com>
2024-03-23 00:22:35 +0900

    2

---
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -0,0 +1,39 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 16,
+        ),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Text(
+                    '\$${expense.amount.toStringAsFixed(2)}'), // 뒤에 숫자를 2자리로 제한
+                const Spacer(), // 공간을 만들어주는 위젯 항상 남는공간을 다 차지함
+                Row(
+                  children: [
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(width: 8),
+                    Text(expense.formattedDate),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
